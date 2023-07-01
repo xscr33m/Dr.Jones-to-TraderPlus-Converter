722af3c283c81c0e2f2064ea59ea1d20a6b281cd
xscr33m <xscr33m@gmail.com>
2023-07-01 22:54:50 +0200

    Projektdateien hinzufügen.

---
+++ Convertion.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Dr.Jones_TraderPlus_Converter
+{
+    internal class Conversion
+    {
+        static void Main(string[] args)
+        {
+            // Pfade zur Eingabe-Textdatei und Ausgabe-JSON-Datei
+            string inputFilePath = args[0];
+            string outputFilePath = Path.ChangeExtension(inputFilePath, ".json");
+
+            // Lese die gesamte Textdatei ein
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            // Initialisiere die Trader-Kategorien-Liste
+            List<TraderCategory> traderCategories = new List<TraderCategory>();
+
+            // Initialisiere die aktuelle Trader-Kategorie
+            TraderCategory currentCategory = null;
+
+            // Durchlaufe alle Zeilen der Textdatei
+            foreach (string line in lines)
+            {
+                // Entferne führende und abschließende Leerzeichen und Tabs
+                string trimmedLine = line.Trim();
+
+                // Ignoriere Kommentarzeilen und Zeilen mit CurrencyName und Trader
+                if (trimmedLine.StartsWith("//") || trimmedLine.StartsWith("<CurrencyName>") || trimmedLine.StartsWith("<Trader>"))
+                    continue;
+
+                // Ignoriere Leerzeilen
+                if (string.IsNullOrWhiteSpace(trimmedLine))
+                    continue;
+
+                // Entferne Kommentare am Ende der Zeile
+                int commentIndex = trimmedLine.IndexOf("//");
+                if (commentIndex >= 0)
+                    trimmedLine = trimmedLine.Substring(0, commentIndex).Trim();
+
+                if (trimmedLine.StartsWith("<Category>"))
+                {
+                    // Neue Kategorie gefunden
+                    string categoryName = trimmedLine.Replace("<Category>", "").Trim();
+                    currentCategory = new TraderCategory { CategoryName = categoryName };
+                    traderCategories.Add(currentCategory);
+                }
+                else if (currentCategory != null && !trimmedLine.StartsWith("<FileEnd>"))
+                {
+                    // Produkt gefunden
+                    string[] productInfo = trimmedLine.Split(',').Select(info => info.Trim()).ToArray();
+                    string productName = productInfo[0];
+                    string ek = "1";
+                    string vk = "-1";
+
+                    if (productInfo.Length >= 3)
+                    {
+                        ek = productInfo[2];
+                        vk = productInfo[3];
+                    }
+
+                    string productData = string.Join(",", productName, "1", "-1", "100", "-1", ek, vk);
+                    currentCategory.Products.Add(productData);
+                }
+            }
+
+            // Erstelle das JSON-Objekt
+            var jsonObject = new
+            {
+                Version = "2.5",
+                EnableAutoCalculation = 0,
+                EnableAutoDestockAtRestart = 0,
+                EnableDefaultTraderStock = 0,
+                TraderCategories = traderCategories
+            };
+
+            // Serialisiere das JSON-Objekt in eine formatierte JSON-Zeichenkette
+            string json = JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
+
+            // Schreibe das JSON in die Ausgabe-Datei
+            File.WriteAllText(outputFilePath, json);
+
+            Console.WriteLine("Die Umwandlung wurde abgeschlossen. Die JSON-Datei wurde erstellt.");
+        }
+    }
+
+    // Klasse, um Trader-Kategorien darzustellen
+    public class TraderCategory
+    {
+        public string CategoryName { get; set; }
+        public List<string> Products { get; set; }
+
+        public TraderCategory()
+        {
+            Products = new List<string>();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,30 @@
+﻿## xscr33m's Dr.Jones→TraderPlus-Converter
+
+Converts any Dr.Jones's Trader.txt into a TraderPlusPriceConfig.json. 
+It ignores the <Trader>,<Currency>,<FileEnd>, and any comment written with "//" like the original.
+
+
+## How-To-Use
+
+    1. Download the latest release of [xscr33m's Dr.Jones→TraderPlus-Converter.zip](https://github.com/xscr33m/xscr33m's Dr.Jones→TraderPlus-Converter/releases).  →→→→→→→→→→→→→→→→→
+    2. Unzip the "xscr33m's Dr.Jones→TraderPlus-Converter.zip" file with 7Zip or another program.
+    3. Open the extracted folder.
+    4. Drag your TraderConfig.txt on the "xscr33m's Dr.Jones→TraderPlus-Converter.exe".
+    5. Your TraderPlusPriceConfig.json will be created in the origin path of TraderConfig.txt.
+
+
+## License
+
+[MIT License](https://spdx.org/licenses/)
+
+
+## Feedback
+
+If you have any feedback, please reach out to me!
+I would appreciate it very much! 
+
+[![Logo](https://cdn.discordapp.com/attachments/1103457256164569120/1109278528354127982/Join_Discord-logo.png)](https://discord.com/invite/PasvscT4Nh)
+
+[![Logo](https://cdn.discordapp.com/attachments/1116457342658170920/1116458450138972230/PayPal.png)](https://www.paypal.com/paypalme/dheil53)
+
+# Made with ♥ for this amazing community!
